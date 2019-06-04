1ed7aa7d0d69b3158c4c3d4ccfe34effdcbd348c
nhurichie <richnhu@gmail.com>
2019-06-04 07:28:09 -0500

    filecorrects_additions_jsandhandlbars

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,33 +1,49 @@
 //API -- AJAX
-$(".eatburger").on("click", function (event) {
 
-  var id = $(this).data("id");
-  var devouredState = {
-    devoured: 1
-
-  };
-  //SEND PUT REQUEST
-  $.ajax("/api/burgers/" + id, {
-    type: "PUT",
-    data: devouredState
-  }).then(function () {
-    console.log("Burger devoured");
-    location.reload();
-  });
-});
-
-//preventDefault
 $(function () {
+  $(".eatburger").on("click", function (event) {
+
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+
+    };
+    //SEND PUT REQUEST
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function () {
+      console.log("Burger devoured");
+      location.reload();
+    });
+  });
+
+  $(".eatburger").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function () {
+      console.log("Burger devoured");
+      location.reload();
+    });
+  });
+
   $(".create-form").on("click", function (event) {
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("newBurger").val().trim(),
+      burger_name: $("newburger").val().trim(),
       devoured: $("[name=burger]:checked").val().trim()
     };
 
     //SEND POST REQUEST
-
     $.ajax("api/burgers" + id, {
       type: "POST",
       data: newBurger
@@ -37,5 +53,4 @@ $(function () {
     });
   });
 
-  //TRASH BURGER
 });
